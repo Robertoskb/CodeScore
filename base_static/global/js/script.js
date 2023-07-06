5ac2882e1ca519bcc105a646ad2ffaabee408fd2
José Roberto <97955337+Robertoskb@users.noreply.github.com>
2023-07-06 16:29:44 -0300

    sidebar and exam cards

---
+++ base_static/global/js/script.js
@@ -0,0 +1,21 @@
+const navBar = document.querySelector("nav"),
+       menuBtns = document.querySelectorAll(".menu-icon"),
+       overlay = document.querySelector(".overlay");
+     menuBtns.forEach((menuBtn) => {
+       menuBtn.addEventListener("click", () => {
+         navBar.classList.toggle("open");
+       });
+     });
+     overlay.addEventListener("click", () => {
+       navBar.classList.remove("open");
+     });
+
+     document.addEventListener('DOMContentLoaded', function() {
+      var menuToggle = document.getElementsByClassName('menu-toggle');
+      
+      Array.from(menuToggle).forEach(function(element) {
+        element.addEventListener('click', function() {
+          this.classList.toggle('active');
+        });
+      });
+    });
--- profiles/models.py
+++ profiles/models.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
 
 
-def get_user_type(self):
-    if len(self.username) == 14:
+def get_user_type(user):
+    if len(user.username) == 14:
         return 'student'
-    if len(self.username) == 7:
+    if len(user.username) == 7:
         return 'teacher'
-    if self.is_superuser:
+    if user.is_superuser:
         return 'admin'
-    if self.is_staff:
+    if user.is_staff:
         return 'staff'
     return 'user'
 
