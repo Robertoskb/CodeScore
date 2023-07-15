a24dc4c8891d0ef7d939bf7e4580c1c4d3402dec
José Roberto <joseroberto2k05@gmail.com>
2023-07-14 23:42:38 -0300

    Update get_exams.py

--- utils/get_exams.py
+++ utils/get_exams.py
@@ -4,12 +4,12 @@ from django.shortcuts import get_object_or_404
 from exams.models import Exam
 
 
-def get_exam(exam_name, check_avaliable=False):
+def get_exam(exam_name, check_available=False):
     exam = get_object_or_404(
         Exam.objects.prefetch_related('questions'),
         slug=exam_name)
 
-    if check_avaliable and exam.avaliable is False:
+    if check_available and exam.available is False:
         raise Http404()
 
     return exam
