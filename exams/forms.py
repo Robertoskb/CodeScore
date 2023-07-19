f9726f9ea1df40ee619beeb960bd6cecb8190530
José Roberto <97955337+Robertoskb@users.noreply.github.com>
2023-07-19 14:09:39 -0300

    update forms

--- exams/forms.py
+++ exams/forms.py
@@ -19,10 +19,33 @@ class PythonFileForm(forms.Form):
 class ExamForm(forms.ModelForm):
     class Meta:
         model = Exam
+
+        labels = {
+            'name': 'Nome da Prova'
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Ex: Primeira Avaliação'
+            }),
+        }
+
         fields = ['name']
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
+        labels = {
+            'name': 'Nome da Questão:',
+            'statement_pdf': 'Enunciado em PDF:',
+            'answer_zip': 'Gabarito em ZIP:',
+        }
+
         fields = ['name', 'statement_pdf', 'answer_zip']
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Ex: Menor Caminho'
+            }),
+        }
